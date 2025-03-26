f1c29563be3650baa28f19dc1ec6955103312126
yujin0124 <yujin0124@khu.ac.kr>
2025-03-26 10:16:07 +0900

    풀이 제출

---
+++ yujin0124/250326/1012_silver2.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+sys.setrecursionlimit(100000)
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+m, n, k = 0, 0, 0
+field = [[0] * 55 for _ in range(55)]
+
+def dfs(x, y):
+  field[x][y] = 0
+  
+  for i in range(4):
+    nx, ny = x + dx[i], y + dy[i]
+    if nx < 0 or nx >= m or ny < 0 or ny >= n:
+      continue
+    if field[nx][ny] == 1:
+      dfs(nx, ny)
+
+
+t = int(input())
+while t > 0:
+  t -= 1
+  
+  m, n, k = map(int, input().split())
+  que = deque()
+  for i in range(k):
+    x, y = map(int, input().split())
+    field[x][y] = 1
+  
+  earthworm = 0
+  for i in range(m):
+    for j in range(n):
+      if field[i][j] == 1:
+        earthworm += 1
+        dfs(i, j)
+
+  print(earthworm)
