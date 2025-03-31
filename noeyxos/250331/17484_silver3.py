023528e2c633105e2d75a3dc36eac606b9c66973
soyeon <thdusmm6@gmail.com>
2025-03-31 22:01:03 +0900

    250331 풀이 다시 제출

--- noeyxos/250331/17484_silver3.py
+++ noeyxos/250331/17484_silver3.py
@@ -4,8 +4,8 @@ sys.setrecursionlimit(10**6)
 n, m = map(int,input().split())
 route =[list(map(int, input().split())) for _ in range(n)]
 
-dr = [-1, 0, 1]
-dc = [1, 1, 1]
+dc = [-1, 0, 1]
+dr = [1, 1, 1]
 
 min_fuel = float('inf')
 
@@ -17,7 +17,6 @@ def dfs(row, col, fuel, prev_direction):
 
     if row == n - 1: 
         min_fuel = min(min_fuel, fuel)
-        print("min_fuel" + min_fuel)
         return
 
     for i in range(3): 
