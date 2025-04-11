68733c5b2551d0af54dc26097d164a74fc4e6865
dahyun <halover111@gmail.com>
2025-04-11 10:42:20 +0900

    17406 추가 풀이

--- dahlbong/250411/17406_gold4.py
+++ dahlbong/250411/17406_gold4.py
@@ -6,8 +6,22 @@ def calculate_min_sum(graph):
     return min(sum(row) for row in graph)
 
 def rotate(graph, r, c, s):
-    
-    return
+    for layer in range(1, s + 1):
+        top = r - layer
+        left = c - layer
+        bottom = r + layer
+        right = c + layer
+
+        prev = graph[top][left]
+
+        for j in range(left + 1, right + 1):
+            graph[top][j], prev = prev, graph[top][j]
+        for i in range(top + 1, bottom + 1):
+            graph[i][right], prev = prev, graph[i][right]
+        for j in range(right - 1, left - 1, -1):
+            graph[bottom][j], prev = prev, graph[bottom][j]
+        for i in range(bottom - 1, top - 1, -1):
+            graph[i][left], prev = prev, graph[i][left]
 
 
 N, M, K = map(int, input().split())
